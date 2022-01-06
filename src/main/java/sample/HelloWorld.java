4995351eaee1004072008931e6e009fe024e4b67
bharatchevireddy <96376221+bharatchevireddy@users.noreply.github.com>
2022-01-06 17:33:48 +0530

    Add files via upload

---
+++ src/main/java/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = LocalTime.now();
+    System.out.println("The current local time is: " + currentTime);
+    //Greeter greeter = new Greeter();
+    System.out.println("Hello Java World");
+  }
+}
